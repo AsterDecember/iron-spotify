3d3b436e1a7bef6ff3b3a915b6dab2e4e02c47ce
AsterDecember <aster.december@gmail.com>
2018-12-03 16:50:00 -0600

    changed callback dynamic

--- middleware/passport.js
+++ middleware/passport.js
@@ -25,7 +25,7 @@ passport.use(
         {
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: 'http://localhost:3000/auth/callback'
+            callbackURL: (process.env.URI || 'http://localhost:3000/auth/callback')
         },
         function(accessToken, refreshToken, expires_in, profile, done) {
             User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
