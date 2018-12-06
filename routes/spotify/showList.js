820640b9e885e2c3bfb8efc7204ffed41776ffab
AsterDecember <aster.december@gmail.com>
2018-12-06 16:24:38 -0600

    views for detail playlist created

--- routes/spotify/showList.js
+++ routes/spotify/showList.js
@@ -2,12 +2,24 @@ var SpotifyWebApi = require('spotify-web-api-node')
 const express = require('express')
 const router = express.Router()
 
+router.get('/:id',(req,res,next)=>{
+    const {id}=req.params
+    //this is a playlist id
+    console.log(id)
+
+    //TODO: Print info from spotify about playlist
+
+
+    res.render('music/spotify/playlistDetail')
+})
+
 router.get('/',(req,res,next)=>{
     console.log(req.app.locals.user)
     const {playlists} = req.app.locals.user
     console.log(playlists)
     res.render('music/spotify/playlists',{playlists})
 })
+
 
 
 module.exports = router
