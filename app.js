f3632b4d41ee87ea7ed0f2df025e9e33c661d574
AsterDecember <aster.december@gmail.com>
2018-12-06 21:05:58 -0600

    log out addet to navbars, favicon added

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
+app.use(favicon(__dirname + '/public/images/randjLogo.png'));
 app.use(passport.initialize());
 app.use(passport.session());
 
